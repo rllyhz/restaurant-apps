04687d1b93927f870cdd0852dd0dcfe1d9067254
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-02-16 05:27:04 +0700

    Created ErrorMessage component

---
+++ src/scripts/components/ErrorMessage.js
@@ -0,0 +1,36 @@
+import '../../styles/error-message.css';
+
+export default class ErrorMessage extends HTMLElement {
+  static tagName = 'error-message';
+
+  set errorData({
+    title, description, retryBtnText, retryBtnCallback,
+  }) {
+    this.title = title;
+    this.description = description;
+    this.retryBtnText = retryBtnText;
+    this.btnCallback = () => {
+      retryBtnCallback();
+    };
+
+    this._render();
+  }
+
+  connectedCallback() {
+    this.querySelector('.retry').addEventListener('click', this.btnCallback);
+  }
+
+  disconnectedCallback() {
+    this.querySelector('.retry').removeEventListener('click', this.btnCallback);
+  }
+
+  _render() {
+    this.innerHTML = `
+      <p class='title'>${this.title}</p>
+      <p class='description'>${this.description}</p>
+      <button class='retry'>${this.retryBtnText}</button>
+    `;
+  }
+}
+
+customElements.define(ErrorMessage.tagName, ErrorMessage);
