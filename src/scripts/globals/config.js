de19ad780c5eb843ae9236d3da3c8697a06d65f6
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-02-16 01:05:36 +0700

    Created config file

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_IMAGE_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
+  BASE_IMAGE_LARGE: 'https://restaurant-api.dicoding.dev/images/large/',
+};
+
+export default CONFIG;
