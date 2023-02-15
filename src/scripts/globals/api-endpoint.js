5f4e5f422c0425f49937ceac4bea72664404c128
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-02-16 01:15:57 +0700

    Created api endpoints file

---
+++ src/scripts/globals/api-endpoint.js
@@ -0,0 +1,9 @@
+import CONFIG from './config';
+
+const API_ENDPOINT = {
+  GET_RESTAURANTS: `${CONFIG.BASE_URL}list`,
+  DETAIL_RESTAURANT: (id) => `${CONFIG.BASE_URL}detail/${id}`,
+  ADD_NEW_REVIEW: `${CONFIG.BASE_URL}review`,
+};
+
+export default API_ENDPOINT;
